f11ef7276dc9b216cccd4374aa293338ff0b8f42
aeduaardo <aeduardo.dev@gmail.com>
2020-07-26 17:41:49 -0300

    docker integration added

--- R_docker/blueprint.py
+++ R_docker/blueprint.py
@@ -1,7 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+import docker
 
 docker_routes = Blueprint("docker", __name__, url_prefix="/docker")
 
 @docker_routes.route("")
 def index():
-    return render_template("docker.html")
+    try:
+        docker_con = docker.DockerClient('127.0.0.1:2376')
+        container = docker_con.containers.get('flask-app')
+        flask_app = {
+            'id': container.short_id,
+            'image': container.image.tags[0],
+            'name': container.name,
+            'status': container.status
+        }
+    except Exception as error:
+        flask_app = None
+    return render_template("docker.html", container = flask_app)
+
+@docker_routes.route("/start")
+def start():
+    try:
+        docker_con = docker.DockerClient('127.0.0.1:2376')
+        container = docker_con.containers.get('flask-app')
+        container.start()
+    except Exception as error:
+        pass
+
+    return redirect(url_for('docker.index'))
+@docker_routes.route("/stop")
+def stop():
+    try:
+        docker_con = docker.DockerClient('127.0.0.1:2376')
+        container = docker_con.containers.get('flask-app')
+        container.stop()
+    except Exception as error:
+        pass
+
+    return redirect(url_for('docker.index'))
